b8fff4973426756c2fef59e3bf1b310701b0355d
Spencer Steffen <spencer@citrusme.com>
2011-09-09 15:03:08 -0700

    add drop ship order table to users#show

--- lib/spree_drop_shipping/custom_hooks.rb
+++ lib/spree_drop_shipping/custom_hooks.rb
@@ -1,8 +1,10 @@
 module SpreeDropShipping
   class CustomHooks < Spree::ThemeSupport::HookListener
     
+    insert_after :account_summary, 'users/drop_ship_orders'
+    
     insert_after :admin_tabs, 'admin/shared/suppliers_tab'
     insert_after :admin_product_form_right, 'admin/shared/product_form_right'
-
+    
   end
 end
