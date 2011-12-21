afc22a817edb61303a2e05906c2a03e0c7f3936e
Spencer Steffen <spencer@citrusme.com>
2011-12-20 18:45:15 -0800

    remove deprecation warning after upgrading cucumber

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -132,7 +132,7 @@ end
 
 When /^(?:|I )fill in the following:$/ do |fields|
   fields.rows_hash.each do |name, value|
-    When %{I fill in "#{name}" with "#{value}"}
+    step %{I fill in "#{name}" with "#{value}"}
   end
 end
 
